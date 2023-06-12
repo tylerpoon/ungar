f239f93fef4433f0c91cecf78a94d706bb5fe0a3
Tyler Poon <tylerpoon@gmail.com>
2023-06-12 16:08:46 -0700

    commit me up before you go

--- src/cfr.rs
+++ src/cfr.rs
@@ -1,3 +1,26 @@
+use super::{
+    abstract_game::AbstractGame,
+    card_abstraction::BucketId,
+    strategy::{ Strategy, Regrets },
+    node::NodeId,
+};
 
-pub fn solve() {
+pub struct CFREngine {
+    abstract_game: AbstractGame,
+    strategy: Strategy,
+    regrets: Regrets,
+}
+
+impl CFREngine {
+    pub fn new(abstract_game: AbstractGame) -> CFREngine {
+        CFREngine {
+            abstract_game,
+            strategy: Strategy::new(),
+            regrets: Regrets::new(), 
+        }
+    }
+
+    pub fn solve(&mut self, node_id: NodeId, bucket_id: BucketId) {
+        //TODO: implement MCCFR here
+    }
 }
